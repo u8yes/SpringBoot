7de64b96dafe0314806a8f024aad5fe1bf77bbfe
u8yes <108450018+u8yes@users.noreply.github.com>
2022-12-01 20:29:16 +0900

    60

---
+++ src/demo/src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.apache.catalina.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping(value = "/user") /* Get이나 SetMapping으로 바로 작성 */
+    public void test(){
+        System.out.println("test() 실행");
+
+        UserDto userDto = new UserDto();
+        userDto.setAge(33);
+        userDto.setName("Jesus_Christ");
+
+        System.out.printf("name : %s, age : %d\n",
+                userDto.getName(), userDto.getAge());
+        
+    }
+}
---
+++ src/demo/src/main/java/com/example/demo/UserDto.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.annotation.processing.Generated;
+
+@Getter @Setter @ToString
+public class UserDto {
+    private String name;
+    private int age;
+}

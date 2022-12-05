724e3489738a1bcb5bc6426f0d87bfbc743a6b8c
u8yes <108450018+u8yes@users.noreply.github.com>
2022-12-05 19:14:02 +0900

    63

---
+++ src/proj-spring/src/main/java/proj/projspring/ProjSpringApplication.java
@@ -0,0 +1,13 @@
+package proj.projspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjSpringApplication.class, args);
+	}
+
+}
